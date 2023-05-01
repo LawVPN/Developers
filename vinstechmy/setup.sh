60fe7246f533f5517b47513477b65b3df9e34fdf
LawVPN <129483274+LawVPN@users.noreply.github.com>
2023-05-01 23:01:23 +0700

    Update setup.sh

--- vinstechmy/setup.sh
+++ vinstechmy/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Add ssh, vmess, and trojan
-wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/add-ssh-user"
-wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/add-vmess-user"
-wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/add-trojan-user"
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/add-ssh-user"
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/add-vmess-user"
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/add-trojan-user"
 
 #Delete ssh, vmess, and trojan
-wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/del-ssh-user"
-wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/del-vmess-user"
-wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/LawVPN/Developers/vinstechmy/del-trojan-user"
+wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/del-ssh-user"
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/del-vmess-user"
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/LawVPN/Developers/main/vinstechmy/del-trojan-user"
 
 #Permission
 chmod +x /usr/local/bin/add-ssh-user
